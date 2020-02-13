e1fabe5aebd0eb32eddaeb06dcb200a372b48f89
ashbowers <X**Wing2814>
2020-02-12 19:55:58 -0500

    created server 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 
-app.use(cors)
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
