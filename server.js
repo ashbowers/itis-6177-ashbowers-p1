e69057afa93bf50ba922a61e54d0679a4d33e9c2
ashbowers <X**Wing2814>
2020-02-19 19:59:39 -0500

    2/19/2020

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors=require('cors')
+const axios=require('axios')
 const app = express()
 const port = 3000
 
 
 app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/lambda', (req, res) => {
+    axios.get('https://sgegy8vi0k.execute-api.us-west-2.amazonaws.com/prod')
+    .then(resp=>{
+        console.log(resp);
+        res.send(resp.data)
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
